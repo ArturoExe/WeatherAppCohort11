2cc07ebe195215a6761645134408bc15aeada475
Arturo <amarturin15@gmail.com>
2026-09-23 17:37:06 -0700

    Initial Commit

---
+++ WeatherAppCohort11/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherAppCohort11
+//
+//  Created by Arturo Martinez on 9/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherAppCohort11/WeatherAppCohort11App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppCohort11App.swift
+//  WeatherAppCohort11
+//
+//  Created by Arturo Martinez on 9/23/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppCohort11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
